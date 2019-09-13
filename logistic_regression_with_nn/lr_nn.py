bcabce740352c66ad34912620df4ac695c5ff9b6
André dos Santos <andre.eds@gmail.com>
2019-09-13 13:53:48 -0600

    Python file for Logistic Regression and update .gitignore

---
+++ logistic_regression_with_nn/lr_nn.py
@@ -0,0 +1,27 @@
+###### OBJECTIVES ######
+# 1. Construct the Coursera Logistic Regression with a NN
+#       - Add task by task
+#       - Learn the funnctions structures
+#       - Explain the relatiom with the theory
+#       - Check outputs on the fly
+
+
+# numpy is the fundamental package for scientific computing with Python.
+# h5py is a common package to interact with a dataset that is stored on an H5 file.
+# matplotlib is a famous library to plot graphs in Python.
+# PIL and scipy are used here to test your model with your own picture at the end.
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from lr_utils import load_dataset
+
+
+
+
+
+# RUN
+if __name__ == "__main__":
+    print("Logistic Regression with a NN")
